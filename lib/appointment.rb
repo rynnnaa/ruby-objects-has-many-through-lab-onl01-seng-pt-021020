a010cc16a9c32daef9bea788f15f884daff08e5b
Ryna Truong <rynatruong@outlook.com>
2020-03-19 20:36:34 +0000

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,25 @@
+class Appointment
+  
+  @@all = []
+  attr_reader
+  attr_accessor :doctor, :patient, :date
+  
+  def initialize(date, patient, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def doctor
+    @doctor
+  end
+  
+  def patient
+    @patient
+  end
+end
---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+  
+  @@all = []
+  attr_reader :name
+  attr_accessor :appointment
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def new_appointment(date, patient)
+    Appointment.new(date, patient, self)
+  end
+  
+  def patients
+    appointments.map {|appt|appt.date}
+  end
+    
+end
